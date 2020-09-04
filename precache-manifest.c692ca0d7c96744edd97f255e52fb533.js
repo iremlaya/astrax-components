dd8344e94a1d2c0acf4338a8ecc7bbe4fe233235
iremlaya <ikaya@std.iyte.edu.tr>
2020-09-04 09:35:13 +0300

    Updates

---
+++ precache-manifest.c692ca0d7c96744edd97f255e52fb533.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9411a773938afa8bb312492037dfe7a8",
+    "url": "/custom-lib/index.html"
+  },
+  {
+    "revision": "fb71fd357a1784f2c8db",
+    "url": "/custom-lib/static/css/main.6f99bdef.chunk.css"
+  },
+  {
+    "revision": "151a3d18eb3f7daa3e26",
+    "url": "/custom-lib/static/js/2.2ab51df4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/custom-lib/static/js/2.2ab51df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb71fd357a1784f2c8db",
+    "url": "/custom-lib/static/js/main.0c21f5be.chunk.js"
+  },
+  {
+    "revision": "dda586d0326d31e7aaba",
+    "url": "/custom-lib/static/js/runtime-main.1c392375.js"
+  },
+  {
+    "revision": "e067dece2955ee32806f0784d883caef",
+    "url": "/custom-lib/static/media/Camera.e067dece.svg"
+  },
+  {
+    "revision": "3d38d5b5ca3f75d2a4d492165b3ffb71",
+    "url": "/custom-lib/static/media/Down.3d38d5b5.svg"
+  },
+  {
+    "revision": "0fda890ce3d5fa11e03919cb46c37bb0",
+    "url": "/custom-lib/static/media/Profile.0fda890c.svg"
+  },
+  {
+    "revision": "51604fd968ff39f9f0d705d93b4cefdf",
+    "url": "/custom-lib/static/media/book1.51604fd9.jpg"
+  },
+  {
+    "revision": "372e30313da54a17f8f3c92fcb017dc4",
+    "url": "/custom-lib/static/media/book2.372e3031.jpg"
+  },
+  {
+    "revision": "3eeba28e905cfe3ea510914c865e9edb",
+    "url": "/custom-lib/static/media/cancel.3eeba28e.svg"
+  },
+  {
+    "revision": "8a70241f22ffbffa2451a39c809f3c5b",
+    "url": "/custom-lib/static/media/check.8a70241f.svg"
+  },
+  {
+    "revision": "f79213439a03822ad837a3f91a6123ba",
+    "url": "/custom-lib/static/media/dropdown-arrow.f7921343.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/custom-lib/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "c8cf4890d9ed542fa9e03cb797c55039",
+    "url": "/custom-lib/static/media/jotform-logo-transparent-writing.c8cf4890.svg"
+  },
+  {
+    "revision": "a2bc2264ef0dbc414d5e89a80887df2f",
+    "url": "/custom-lib/static/media/search-bold.a2bc2264.svg"
+  },
+  {
+    "revision": "6075887403c966a4599dc1ecebccbfd7",
+    "url": "/custom-lib/static/media/search-fade.60758874.svg"
+  }
+]);
